369a366998971740cebb9c7e109e7d5ded5f4f86
Lam <tsangtaklam@yahoo.com.hk>
2025-02-19 02:55:12 +0800

    third change

--- src/app/KMB/components/SeachBar.js
+++ src/app/KMB/components/SeachBar.js
@@ -11,7 +11,7 @@ export const SearchBar = ({ Search, setSearch }) => {
     return (
       <div className="flex justify-center gap-2 mt-10">
         <input 
-          className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          className="border-2 border-black transition duration-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
           placeholder="請輸入車站名稱"
           type="text"
           value={Search}
