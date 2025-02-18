d5084fedd0228868d87cb347cee8b7c85e7cf1be
Lam <tsangtaklam@yahoo.com.hk>
2025-02-19 02:19:06 +0800

    second change

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-50 bg-[url('./image/img.jpg')] bg-cover">
       
-      <h1 className="text-4xl font-bold mb-6 text-gray-800">歡迎來到交通應用</h1>
+      <h1 className="text-4xl font-bold mb-6 text-black-800">歡迎來到交通應用</h1>
       <div className="flex justify-center gap-10 text-2xl">
         <Link
           className="transition-transform duration-300 ease-in-out transform hover:scale-105 hover:text-red-600"
